fd94bc6eae7e2a54e4d1fb3dfe78abb3971b22a5
Damitha <hellodamitha@gmail.com>
2023-04-19 13:14:06 +0530

    hh

---
+++ q22/q22/EncapClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q22
+{
+    internal class EncapClass
+    {
+        private int age;
+        private string name;
+        public void setAge(int ageFromUser)
+        {
+            age = ageFromUser;
+        }
+        public int getAge()
+        {
+            return age;
+        }
+
+        public void setName(string ageFromUser)
+        {
+            name = ageFromUser;
+        }
+        public string getName()
+        {
+            return name;
+        }
+
+    }
+}
---
+++ q22/q22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EncapClass obj = new EncapClass();
+            obj.setAge(20);
+            obj.setName("Damitha");
+
+            Console.WriteLine("Age is: " +obj.getAge());
+            Console.WriteLine("Name is: " + obj.getName());
+            Console.ReadKey();
+        }
+    }
+}
