0516e4f7fb6990ccde5bd76d1480582279d8c4a5
Damitha <hellodamitha@gmail.com>
2023-05-10 10:48:24 +0530

    gh

---
+++ q35/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             int num1 = int.Parse(textBox1.Text);
+             int num2 = int.Parse(textBox2.Text);
+
+             int sum = num1 + num2;
+             label4.Text = "Sum : " + sum.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            int sub = num1 - num2;
+            label4.Text = "Substraction : " + sub.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            int mul = num1 * num2;
+            label4.Text = "Multiplication : " + mul.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            int div = num1 / num2;
+            label4.Text = "Division : " + div.ToString();
+        }
+    }
+}
