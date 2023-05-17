a875b0aea76902c7b2ec9adca4ad4716a1e657d2
Damitha <hellodamitha@gmail.com>
2023-05-17 20:33:48 +0530

    df

---
+++ q38/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Liter Value : ");
+                int liter = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Mililiter : " + (liter * 1000));
+            }
+            catch (FormatException ex) 
+            {
+                Console.WriteLine( "System Error: " + ex.Message );
+            
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ q39/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Projects\Uni\C#\Lec\q39\MyTestDB.mdf;Integrated Security=True;Connect Timeout=30";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            int ID = int.Parse(textBox1.Text);
+
+            SqlConnection con = new SqlConnection(connectionString);
+            string query = "INSERT INTO info (ID) VALUES (ID)";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            try
+            {
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Inserted Successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
