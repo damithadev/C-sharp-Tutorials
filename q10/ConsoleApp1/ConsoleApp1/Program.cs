5387637368246c0f00ec60f5f24386f7e82b0bdf
Damitha <hellodamitha@gmail.com>
2023-05-27 15:39:29 +0530

    dfg

---
+++ q10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[2, 4]; // Declare a 2-dimensional array with 2 rows and 4 columns
+
+            // Get user inputs for array elements
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write($"Enter element at position [{i}, {j}]: ");
+                    string input = Console.ReadLine();
+                    int number;
+                    if (int.TryParse(input, out number))
+                    {
+                        array[i, j] = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid integer.");
+                        j--; // Decrement j to re-ask for the same column
+                    }
+                }
+            }
+
+            // Display the array elements in a 2x4 matrix
+            Console.WriteLine("Array elements:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- q10/ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
+++ q10/ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Q9")]
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("Q9")]
-[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b1a81a1f-bf85-49bd-9c55-29e9299967dd")]
+[assembly: Guid("100a487c-6402-4947-9f52-41cf6c9f3a10")]
 
 // Version information for an assembly consists of the following four values:
 //
