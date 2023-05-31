e03a691006ad013e31084f2ea55657feaae153f7
Damitha <hellodamitha@gmail.com>
2023-05-31 12:40:25 +0530

    jh

---
+++ q39/WindowsFormsApp1/WindowsFormsApp1/Form3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Projects\Uni\C#\Lec\q39\MyTestDB.mdf;Integrated Security=True;Connect Timeout=30";
+            string query = "SELECT * FROM Info";
+
+            SqlDataAdapter da = new SqlDataAdapter(query, connectionString);
+            DataSet ds = new DataSet();
+
+            da.Fill(ds, "Info");
+            dataGridView1.DataSource = ds.Tables["Info"];
+        }
+    }
+}
--- q39/WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ q39/WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,7 +18,10 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form3());
+
+            /*
+            */
         }
     }
 }
